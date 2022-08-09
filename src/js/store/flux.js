99f24c6068fea417ebc0891920d798e9ce19f7c8
ec1086 <ec1086@gmail.com>
2022-08-09 00:48:46 +0000

    fetch init

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,69 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
+  return {
+    store: {
+      
+		questions: [],
+		
+		
+		
+		
+		
+		
+		demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+    },
+    actions: {
+
+      getQuiz: () => {
+        const store = getStore();
+        fetch(
+          "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=boolean"
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            console.log(data.results[2].question)
+            store.questionList = data
+          });
+          
+            
+      },
+
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
+      loadSomeData: () => {
+        /**
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+      },
+      changeColor: (index, color) => {
+        //get the store
+        const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+        //we have to loop the entire demo array to look for the respective index
+        //and change its color
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+        //reset the global store
+        setStore({ demo: demo });
+      },
+    },
+  };
 };
 
 export default getState;
--- src/js/views/quiz.js
+++ src/js/views/quiz.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "../../styles/home.css";
 import { Link } from "react-router-dom";
-
+import { Context } from "../store/appContext";
 
 export const Quiz = () => {
-    return (
-<div> insert questions here! 
-</div>
-)};
+  const { store, actions } = React.useContext(Context);
+  const [question, setQuestion] = React.useState("");
+  function getQuestion() {
+    setQuestion(store.questionList.results[1].question);
+  }
+
+  return (
+    <div>
+      <p>{question}</p>
+      <button onClick={getQuestion}>Next Question</button>
+    </div>
+  );
+};
